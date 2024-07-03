73a85d2f8e7cb644c2a012ccf795a4f3f0ac2a83
Zara Khan <ZaraMustafa@ymail.com>
2024-07-02 23:19:15 -0400

    deploy

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1 className="text-3xl font-bold text-orange-400 underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
